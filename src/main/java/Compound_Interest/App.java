b9e4e99e4adcefed4542869bb783f4fcbfe90d26
ryanpersad <Rpersad0305@gmail.com>
2021-06-06 20:13:56 -0400

    update

--- src/main/java/Compound_Interest/App.java
+++ src/main/java/Compound_Interest/App.java
@@ -1,10 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ryan Persad
+ */
 package Compound_Interest;
 import java.util.Scanner;
 import java.lang.Math;
-/*
- *  UCF COP3330 Summer 2021 Assignment 13 Solution
- *  Copyright 2021 Ryan Persad
- */
+
+
 public class App
 {
     public static void main( String[] args )
